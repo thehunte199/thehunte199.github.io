dd2c0481379bafc3c52def4a4324c79c363612cb
thehunte199 <thehunte199@users.noreply.github.com>
2020-08-04 17:32:43 -0400

    getting rid of the email form and removing unnecesary files

--- forms/contact.php
+++ forms/contact.php
@@ -1,45 +1,4 @@
 <?php
-  // good news, I'm an actual developer and can write my own php
-  // /**
-  // * Requires the "PHP Email Form" library
-  // * The "PHP Email Form" library is available only in the pro version of the template
-  // * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  // * For more info and help: https://bootstrapmade.com/php-email-form/
-  // */
-
-  // // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = 'contact@jamesward.tech';
-
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
-
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
-  
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
-
-  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  // /*
-  // $contact->smtp = array(
-  //   'host' => 'example.com',
-  //   'username' => 'example',
-  //   'password' => 'pass',
-  //   'port' => '587'
-  // );
-  // */
-
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
-
-  // echo $contact->send();
-
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
